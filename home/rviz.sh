77fb92c29bda266dd4acf63f77955f944afa10de
ymd-stella <world.applepie@gmail.com>
2019-08-03 21:14:21 +0900

    Update rviz.sh

--- home/rviz.sh
+++ home/rviz.sh
@@ -1,7 +1,8 @@
 
 tmux split-window "source /opt/ros/${ROS_DISTRO_DOCKER}/setup.bash && \
      roscore"
-tmux split-window "source ros2_ws/install/setup.bash && \
+tmux split-window "source /opt/ros/${ROS2_DISTRO_DOCKER}/setup.bash && \
+     source ros2_ws/install/setup.bash && \
      export ROS_MASTER_URI=http://localhost:11311 && \
      ros2 run ros1_bridge dynamic_bridge --bridge-all-2to1-topics"
 tmux select-layout even-vertical
